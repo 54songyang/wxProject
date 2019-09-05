bb979c78959c2470d3bebfd1457423c80c88bd4b
itw_songyang <itw_songyang@tkmail.com>
2019-09-05 15:41:23 +0800

    文章详情页

---
+++ pages/pageDetail/data.js
@@ -0,0 +1,35 @@
+export var data = {
+  "respCode": "01",
+  "respBizMsg": {
+    "title":"文章1",
+    "author":"作者1",
+    "address":"朝阳区",
+    "date":"2019-09-05",
+    "pageType":"天下谜团",
+    "answerNum":"3",
+    "readNum":"12",
+    "pageId":'2023',
+    "pageBg":"http://img4.imgtn.bdimg.com/it/u=3135402106,3540163108&fm=26&gp=0.jpg",
+    "articleDetails":'<p>文章编辑及显示测试文章编辑及显示测试文章编辑及显示测试</p><p><img src="http://img5.imgtn.bdimg.com/it/u=3976365452,3683195683&amp;fm=26&amp;gp=0.jpg"></p><p>文章编辑及显示测试文章编辑及显示测试文章编辑及显示测试</p><p>文章编辑及显示测试文章编辑及显示测试</p><p>文章编辑及显示测试文章编辑及显示测试</p><p>文章编辑及显示测试文章编辑及显示测试</p><p>文章编辑及显示测试文章编辑及显示测试</p>',
+    "answerList":[
+      {
+        'answerPhoto':'http://img3.imgtn.bdimg.com/it/u=4285611627,3487298056&fm=26&gp=0.jpg',
+        "answerName":'回答者1',
+        "answerContent":"评论回答评论回答评论回答评论回答评论回答",
+        "answerDate":"2019-09-05",
+      },
+      {
+        'answerPhoto': 'http://img3.imgtn.bdimg.com/it/u=4285611627,3487298056&fm=26&gp=0.jpg',
+        "answerName": '回答者1',
+        "answerContent": "评论回答评论回答评论回答评论回答评论回答",
+        "answerDate": "2019-09-05",
+      },
+      {
+        'answerPhoto': 'http://img3.imgtn.bdimg.com/it/u=4285611627,3487298056&fm=26&gp=0.jpg',
+        "answerName": '回答者1',
+        "answerContent": "评论回答评论回答评论回答评论回答评论回答",
+        "answerDate": "2019-09-05",
+      },
+    ]
+  }
+}
--- pages/pageDetail/pageDetail.js
+++ pages/pageDetail/pageDetail.js
@@ -1,9 +1,113 @@
 Page({
   data: {
+    pageId:'',
+    title: "",
+    author: "",
+    address: "",
+    date: "",
+    pageType: "",
+    answerNum: "",
+    readNum: "",
+    articleDetails: "",
+    answerList: [],
+    article: '',
+    pageBg: '',
+    comment: "",
   },
-  onLoad: function () {
+  onLoad: function (e) {
+    var title;
+    switch (e.pageType) {
+      case '0':
+        title = '民间诡事'
+        break;
+      case '1':
+        title = '天下谜团'
+        break;
+      case '2':
+        title = "网友怪谈"
+    }
     wx.setNavigationBarTitle({
-      title: '文章1' 
+      title: title
     })
+    this.getInfo(e);
+  },
+  getInfo: function (val) {
+    const that = this;
+    console.log(val);
+    var info = require('./data').data.respBizMsg
+    that.setData({
+      pageId:info.pageId,
+      title: info.title,
+      author: info.author,
+      address: info.address,
+      date: info.date,
+      pageType: info.pageType,
+      answerNum: info.answerNum,
+      readNum: info.readNum,
+      articleDetails: info.articleDetails,
+      answerList: info.answerList,
+      pageBg: info.pageBg
+    })
+    const WxParse = require('../../wxParse/wxParse.js');
+    WxParse.wxParse('article', 'html', info.articleDetails, that, 5)
+    // wx.request({
+    //   url: 'https://www.easy-mock.com/mock/5c1c59326fedb679d1b94a74/hwp-h5/wx-list',
+    //   method: "POST",
+    //   data: val,
+    //   header: {
+    //     'Content-Type': 'json'
+    //   },
+    //   success: function (res) {
+    //     console.log(res);
+
+    //     if (res.data.respCode === '01') {
+
+    //     }
+    //   },
+    //   fail: function () {
+    //     console.log("接口调用失败");
+    //   }
+    // })
+  },
+  toHome: function () {
+    wx.switchTab({
+      url: '../index/index'
+    })
+  },
+  setComment: function (e) {
+    this.setData({
+      comment: e.detail.value
+    })
+  },
+  sendInfo: function (e) {
+    let that = this;
+    if (that.data.comment) {
+      const userInfo = e.detail.userInfo;
+      let data = {
+        pageId:that.data.pageId,
+        userInfo,
+        comment:that.data.comment
+      }
+      console.log(data);
+      // !!!返回内容为评论列表
+      // wx.request({
+      //   url: 'https://www.easy-mock.com/mock/5c1c59326fedb679d1b94a74/hwp-h5/wx-list',
+      //   method: "POST",
+      //   data: data,
+      //   header: {
+      //     'Content-Type': 'json'
+      //   },
+      //   success: function (res) {
+      //     console.log(res);
+
+      //     if (res.data.respCode === '01') {
+
+      //     }
+      //   },
+      //   fail: function () {
+      //     console.log("接口调用失败");
+      //   }
+      // })
+    }
   },
 })
